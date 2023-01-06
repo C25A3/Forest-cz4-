c68b594bf6ad2929efa52ff8d8cea992256db8fe
Wojtek <zxcvweop@gmail.com>
2023-01-06 18:35:43 +0100

    uptade nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,13 @@
 const burgerBtn = document.querySelector('.burger-btn')
 const mobileNav = document.querySelector('.nav-mobile')
+const mobileNavLinks = document.querySelectorAll('.nav-mobile__link')
 
 const mobileNavHandle = () => {
 	burgerBtn.childNodes[1].classList.toggle('burger-btn__bars--active')
-    mobileNav.classList.toggle('nav-mobile--active')
+	mobileNav.classList.toggle('nav-mobile--active')
 }
 
-
 burgerBtn.addEventListener('click', mobileNavHandle)
+mobileNavLinks.forEach(link => {
+	link.addEventListener('click', mobileNavHandle)
+})
