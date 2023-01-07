54742ed2c47965c45d6fcefe8698b534b5e2e775
Wojtek <zxcvweop@gmail.com>
2023-01-07 11:25:03 +0100

    end main page

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,27 @@
+//NAV
 const burgerBtn = document.querySelector('.burger-btn')
 const mobileNav = document.querySelector('.nav-mobile')
 const mobileNavLinks = document.querySelectorAll('.nav-mobile__link')
+//Footer
+const footerYear = document.querySelector('.year')
+
 
 const mobileNavHandle = () => {
 	burgerBtn.childNodes[1].classList.toggle('burger-btn__bars--active')
 	mobileNav.classList.toggle('nav-mobile--active')
 }
 
+
+
+const getYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.textContent = year
+}
+
+
 burgerBtn.addEventListener('click', mobileNavHandle)
 mobileNavLinks.forEach(link => {
 	link.addEventListener('click', mobileNavHandle)
 })
+
+getYear()
