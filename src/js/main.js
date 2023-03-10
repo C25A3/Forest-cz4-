241841e7c7147f7a08f3bdd1ae3bc3e30a4498a7
Wojtek <zxcvweop@gmail.com>
2023-03-10 13:05:38 +0100

    add contact section

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,38 @@
-//NAV
 const burgerBtn = document.querySelector('.burger-btn')
 const mobileNav = document.querySelector('.nav-mobile')
 const mobileNavLinks = document.querySelectorAll('.nav-mobile__link')
-//Footer
-const footerYear = document.querySelector('.year')
-
+const scrollSpySections = document.querySelectorAll('.section')
+const navLinks = document.querySelectorAll('.nav__link-desktop')
+const footerYear = document.querySelectorAll('.year')
 
 const mobileNavHandle = () => {
 	burgerBtn.childNodes[1].classList.toggle('burger-btn__bars--active')
 	mobileNav.classList.toggle('nav-mobile--active')
 }
 
-
-
 const getYear = () => {
 	const year = new Date().getFullYear()
-	footerYear.textContent = year
+	footerYear.forEach(el => el.textContent = year)
 }
 
+const handleScrollSpy = () => {
+	if (document.body.classList.contains('main-page')) {
+		const sections = []
+		scrollSpySections.forEach(section => {
+			if (window.scrollY <= section.offsetTop + section.offsetHeight - 88) {
+				sections.push(section)
+
+				const activeSection = document.querySelector(`[href*="${sections[0].id}"]`)
+				console.log(activeSection);
+				navLinks.forEach(item => item.classList.remove('nav__link-desktop--active'))
+
+				activeSection.classList.add('nav__link-desktop--active')
+			}
+		})
+	}
+}
+
+window.addEventListener('scroll', handleScrollSpy)
 
 burgerBtn.addEventListener('click', mobileNavHandle)
 mobileNavLinks.forEach(link => {
